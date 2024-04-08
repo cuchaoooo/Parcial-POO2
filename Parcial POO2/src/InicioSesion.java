10d5416e8731eb1c3db62f424a64fc428c8c26f1
Manuel <msortiz2509@gmail.com>
2024-04-08 15:11:19 -0500

    cambio incio sesion

--- Parcial POO2/src/InicioSesion.java
+++ Parcial POO2/src/InicioSesion.java
@@ -51,8 +51,8 @@ class InicioSesion extends JFrame {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets = new Insets(30, 10, 10, 10); // Ajustar el margen superior del título
-        loginPanel.add(loginTitleLabel, gbc); // Agregando el título al panel de inicio de sesión
+        gbc.insets = new Insets(30, 10, 10, 10);
+        loginPanel.add(loginTitleLabel, gbc);
         gbc.gridy++;
         loginPanel.add(emailField, gbc);
         gbc.gridy++;
@@ -103,3 +103,4 @@ class InicioSesion extends JFrame {
         SwingUtilities.invokeLater(InicioSesion::new);
     }
 }
+
